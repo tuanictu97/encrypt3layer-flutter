68d19c68eed673c6d68437123cf63b37a5a35068
tuanictu97 <dtc155d4801030056@ictu.edu.vn>
2019-12-06 11:18:15 +0700

    add example run c/c++ on flutter

--- lib/encrypt3layer.dart
+++ lib/encrypt3layer.dart
@@ -1,6 +1,23 @@
 import 'dart:async';
+import 'dart:ffi';  // For FFI
+import 'dart:io';   // For Platform.isX
+
 
 import 'package:flutter/services.dart';
+
+
+// test native for Android
+final DynamicLibrary nativeAddLib = DynamicLibrary.open("libnative_add.so");
+
+//final DynamicLibrary nativeAddLib = Platform.isAndroid
+//    ? DynamicLibrary.open("libnative_add.so")
+//    : DynamicLibrary.process();
+
+final int Function(int x, int y) nativeAdd =
+nativeAddLib
+    .lookup<NativeFunction<Int32 Function(Int32, Int32)>>("native_add")
+    .asFunction();
+
 
 class Encrypt3layer {
   static const MethodChannel _channel =
